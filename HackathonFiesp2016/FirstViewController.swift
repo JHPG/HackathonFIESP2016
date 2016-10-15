c7d5793db07fa739bee7209f7bd1422a621dc173
Jorge H. P. Garcia <j.hrique@hotmail.com>
2016-10-15 20:00:47 -0300

    Tela restaurante ok

--- HackathonFiesp2016/FirstViewController.swift
+++ HackathonFiesp2016/FirstViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet var cards: [UIView]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        for c in cards {
+            let view = c
+            let shadowPath = UIBezierPath(rect: view.bounds)
+            view.layer.masksToBounds = false
+            view.layer.shadowColor = UIColor.black.cgColor
+            view.layer.shadowOffset = CGSize(width: 0, height: 0.5)
+            view.layer.shadowOpacity = 0.2
+            view.layer.shadowPath = shadowPath.cgPath
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,12 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        
+    }
 
 }
 
